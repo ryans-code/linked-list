b0f8d25e974f17e55fb9318381e0610baee6d955
Ryan <ryan.hiner@gmail.com>
2023-02-18 18:35:51 -0600

    more edits

--- ListMain.cpp
+++ ListMain.cpp
@@ -32,130 +32,28 @@ void display()
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));        // randomize RNG seed
-    int size = atoi(argv[1]); // store command line argument as variable
+    srand(time(NULL)); // randomize RNG seed
 
-    if (size < 1)
+    if (argc < 2)
     {
         cout << "Program should be run with command line argument, " << endl
-             << "try './a.out x' where x is an integer number greater than 0." << endl;
-        return -1;
+             << "try './LinkedList x' where x is an integer number greater than 0." << endl;
+        return 0;
     }
-
-    NumList list;
-
-    for (int i = 0; i < size; i++)
+    else
     {
-        list.appendNode(randomNum());
-    }
+        int size = atoi(argv[1]); // store command line argument as variable
 
-    display();
-    int userSelection = 0;
-    bool isSorted = false;
-    cout << endl
-         << "Please make a selection: ";
-    cin >> userSelection;
+        NumList list;
 
-    while (userSelection < 0 || userSelection > 6)
-    {
-        cout << "Make a valid selection: ";
-        cin >> userSelection;
-    }
-
-    while (userSelection != 0)
-    {
-        switch (userSelection)
+        for (int i = 0; i < size; i++)
         {
-        case 1:
-            cout << endl
-                 << "Displaying List: " << endl
-                 << endl;
-            list.displayList();
-            break;
-        case 2:
-            cout << endl
-                 << "List has been sorted!" << endl;
-            list.sortList();
-            isSorted = true;
-            break;
-        case 3:
-            cout << endl
-                 << "List has been reversed!" << endl;
-            list.reverseList();
-            break;
-        case 4:
-            double appendNum;
-            cout << endl
-                 << "Enter a number to append: ";
-            cin >> appendNum;
-            list.appendNode(appendNum);
-            isSorted = false;
-            cout << endl
-                 << appendNum << " has been appended to the list!" << endl;
-            break;
-        case 5:
-            if (isSorted == false)
-            {
-                char userYorN;
-                cout << endl
-                     << "The list must be sorted before inserting a new node into position." << endl
-                     << endl
-                     << "Sort now? (Y or N): ";
-                cin >> userYorN;
-                while (userYorN != 'y' && userYorN != 'Y' && userYorN != 'n' && userYorN != 'N')
-                {
-                    cout
-                        << "Make a valid selection: ";
-                    cin >> userYorN;
-                }
-                if (userYorN == 'y' || userYorN == 'Y')
-                {
-                    list.sortList();
-                    cout << endl
-                         << "List has been sorted!";
-                    isSorted = true;
-
-                    double insertNum;
-                    cout << endl
-                         << "Enter a number to insert: ";
-                    cin >> insertNum;
-                    list.insertNode(insertNum);
-                    cout << endl
-                         << insertNum << " has been inserted!" << endl;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            else
-            {
-                double insertNum;
-                cout << endl
-                     << "Enter a number to insert: ";
-                cin >> insertNum;
-                list.insertNode(insertNum);
-                cout << endl
-                     << insertNum << " has been inserted!" << endl;
-            }
-            break;
-        case 6:
-            double removeNum;
-            cout << endl
-                 << "Enter a number to remove: ";
-            cin >> removeNum;
-            if (list.contains(removeNum))
-            {
-                list.deleteNode(removeNum);
-                cout << endl
-                     << removeNum << " has been removed from the list!" << endl;
-            }
-            else
-                cout << "That number is not in the list!";
-            break;
+            list.appendNode(randomNum());
         }
 
         display();
+        int userSelection = 0;
+        bool isSorted = false;
         cout << endl
              << "Please make a selection: ";
         cin >> userSelection;
@@ -165,11 +63,116 @@ int main(int argc, char *argv[])
             cout << "Make a valid selection: ";
             cin >> userSelection;
         }
+
+        while (userSelection != 0)
+        {
+            switch (userSelection)
+            {
+            case 1:
+                cout << endl
+                     << "Displaying List: " << endl
+                     << endl;
+                list.displayList();
+                break;
+            case 2:
+                cout << endl
+                     << "List has been sorted!" << endl;
+                list.sortList();
+                isSorted = true;
+                break;
+            case 3:
+                cout << endl
+                     << "List has been reversed!" << endl;
+                list.reverseList();
+                break;
+            case 4:
+                double appendNum;
+                cout << endl
+                     << "Enter a number to append: ";
+                cin >> appendNum;
+                list.appendNode(appendNum);
+                isSorted = false;
+                cout << endl
+                     << appendNum << " has been appended to the list!" << endl;
+                break;
+            case 5:
+                if (isSorted == false)
+                {
+                    char userYorN;
+                    cout << endl
+                         << "The list must be sorted before inserting a new node into position." << endl
+                         << endl
+                         << "Sort now? (Y or N): ";
+                    cin >> userYorN;
+                    while (userYorN != 'y' && userYorN != 'Y' && userYorN != 'n' && userYorN != 'N')
+                    {
+                        cout
+                            << "Make a valid selection: ";
+                        cin >> userYorN;
+                    }
+                    if (userYorN == 'y' || userYorN == 'Y')
+                    {
+                        list.sortList();
+                        cout << endl
+                             << "List has been sorted!";
+                        isSorted = true;
+
+                        double insertNum;
+                        cout << endl
+                             << "Enter a number to insert: ";
+                        cin >> insertNum;
+                        list.insertNode(insertNum);
+                        cout << endl
+                             << insertNum << " has been inserted!" << endl;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    double insertNum;
+                    cout << endl
+                         << "Enter a number to insert: ";
+                    cin >> insertNum;
+                    list.insertNode(insertNum);
+                    cout << endl
+                         << insertNum << " has been inserted!" << endl;
+                }
+                break;
+            case 6:
+                double removeNum;
+                cout << endl
+                     << "Enter a number to remove: ";
+                cin >> removeNum;
+                if (list.contains(removeNum))
+                {
+                    list.deleteNode(removeNum);
+                    cout << endl
+                         << removeNum << " has been removed from the list!" << endl;
+                }
+                else
+                    cout << "That number is not in the list!";
+                break;
+            }
+
+            display();
+            cout << endl
+                 << "Please make a selection: ";
+            cin >> userSelection;
+
+            while (userSelection < 0 || userSelection > 6)
+            {
+                cout << "Make a valid selection: ";
+                cin >> userSelection;
+            }
+        }
+
+        cout << endl
+             << "gg" << endl
+             << endl;
+
+        return 0;
     }
-
-    cout << endl
-         << "gg" << endl
-         << endl;
-
-    return 0;
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 #makefile
 
-a.out: ListMain.o NumList.o
-	g++ ListMain.o NumList.o
+LinkedList: ListMain.o NumList.o
+	g++ ListMain.o NumList.o -o LinkedList
 
 ListMain.o: ListMain.cpp NumList.h
 	g++ -c ListMain.cpp
